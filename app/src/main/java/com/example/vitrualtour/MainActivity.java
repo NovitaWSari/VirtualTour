5741c90289f7c79b432474cc333dee42f0bcd91c
NovitaWSari <novitaasari2811@gmail.com>
2024-05-31 10:00:49 +0700

    perkembangan terbaru

--- app/src/main/java/com/example/vitrualtour/MainActivity.java
+++ app/src/main/java/com/example/vitrualtour/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.vitrualtour;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        textView.setText("Home");
+                        return true;
+                    case R.id.putra:
+                        textView.setText("Putra");
+                        return true;
+                    case R.id.putri:
+                        textView.setText("Putri");
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
