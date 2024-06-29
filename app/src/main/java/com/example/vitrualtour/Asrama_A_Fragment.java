bc9707cfbdd23fd78bbc0d8f6d7f261538e09a0e
NovitaWSari <novitaasari2811@gmail.com>
2024-06-29 10:31:53 +0700

    isi layout asrama

--- app/src/main/java/com/example/vitrualtour/Asrama_A_Fragment.java
+++ app/src/main/java/com/example/vitrualtour/Asrama_A_Fragment.java
@@ -17,13 +17,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 import java.util.List;
 
 public class Asrama_A_Fragment extends Fragment {
 
     private ImageView image360;
-    private ImageView iconLocation;
     private TextView address;
+    private MaterialButton directionOneBtn;
+    private MaterialButton directionTwoBtn;
+
+    private String latitudeSaygon = "-7.74777128178355";  // Koordinat Saygon
+    private String longitudeSaygon = "112.73866026944174"; // Koordinat Saygon
+    private String photo360Url = "https://www.google.com/maps/place/Asrama+I+Ponpes+Ngalah/@-7.7582717,112.7297118,3a,75y,340h,100t/data=!3m8!1e1!3m6!1sAF1QipPisY3Fbu4ja9dpfYsP2m08QrHaeoD_DfdKBPT6!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPisY3Fbu4ja9dpfYsP2m08QrHaeoD_DfdKBPT6%3Dw900-h600-k-no-pi-10-ya1-ro0-fo90!7i8704!8i4352!4m13!1m2!2m1!1sasrama+i!3m9!1s0x2dd7d38634bbf737:0x9e31f323b7c458f6!8m2!3d-7.7582717!4d112.7297118!10e5!14m1!1BCgIgARICEAI!15sCghhc3JhbWEgaZIBE3Jlc2lkZW50aWFsX2NvbGxlZ2XgAQA!16s%2Fg%2F11ks2nhz29?coh=205410&entry=ttu";  // URL foto 360
 
     @Nullable
     @Override
@@ -32,59 +39,49 @@ public class Asrama_A_Fragment extends Fragment {
 
         image360 = view.findViewById(R.id.image_360);
         address = view.findViewById(R.id.address);
-        iconLocation = view.findViewById(R.id.icon_address);
+        directionOneBtn = view.findViewById(R.id.directionOneBtn);
+        directionTwoBtn = view.findViewById(R.id.directionTwoBtn);
 
-        image360.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view360Image();
-            }
+        directionOneBtn.setOnClickListener(v -> {
+            directionFromCurrentMap(latitudeSaygon, longitudeSaygon);
         });
 
-        address.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGoogleMaps();
-            }
+        directionTwoBtn.setOnClickListener(v -> {
+            directionBetweenTwoMap(latitudeSaygon, longitudeSaygon, latitudeSaygon, longitudeSaygon); // Menjaga method tetap aktif
         });
 
-        iconLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openLocationInMap(-7.7577463326634195, 112.72877050807833); // Ganti dengan koordinat Anda
-            }
+        image360.setOnClickListener(v -> {
+            view360Image();
+        });
+
+        address.setOnClickListener(v -> {
+            openGoogleMaps();
         });
 
         return view;
     }
 
-    private void view360Image() {
-        // Fungsi ini kosong sementara karena masalah dependensi foto 360
+    private void directionFromCurrentMap(String destinationLatitude, String destinationLongitude) {
+        Uri mapUri = Uri.parse("https://maps.google.com/maps?daddr=" + destinationLatitude + "," + destinationLongitude);
+        Intent intent = new Intent(Intent.ACTION_VIEW, mapUri);
+        startActivity(intent);
     }
 
-    private void openGoogleMaps() {
-        String googleMapsUrl = "https://maps.app.goo.gl/xi2TRraaTjbtBiim6";
+    private void directionBetweenTwoMap(String sourceLatitude, String sourceLongitude, String destinationLatitude, String destinationLongitude) {
+        Uri mapUri = Uri.parse("https://maps.google.com/maps?saddr=" + sourceLatitude + "," + sourceLongitude + "&daddr=" + destinationLatitude + "," + destinationLongitude);
+        Intent intent = new Intent(Intent.ACTION_VIEW, mapUri);
+        startActivity(intent);
+    }
 
-        // Membuat Uri dari Google Maps URL
-        Uri uri = Uri.parse(googleMapsUrl);
-
-        // Membuat Intent untuk membuka aplikasi Google Maps
+    private void view360Image() {
+        Uri uri = Uri.parse(photo360Url);
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         intent.setPackage("com.google.android.apps.maps");
 
-        // Memeriksa apakah aplikasi Google Maps terpasang
         PackageManager packageManager = requireActivity().getPackageManager();
         List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
         boolean isIntentSafe = activities.size() > 0;
 
-        // Menampilkan daftar aplikasi yang bisa menangani intent
-        for (ResolveInfo resolveInfo : activities) {
-            String packageName = resolveInfo.activityInfo.packageName;
-            String activityName = resolveInfo.activityInfo.name;
-            Log.d("IntentHandler", "Found app to handle intent: " + packageName + ", " + activityName);
-        }
-
-        // Jika aplikasi Google Maps terpasang, maka buka alamat tersebut
         if (isIntentSafe) {
             startActivity(intent);
         } else {
@@ -92,29 +89,19 @@ public class Asrama_A_Fragment extends Fragment {
         }
     }
 
-    private void openLocationInMap(double latitude, double longitude) {
-        // Membuat URI untuk lokasi
-        Uri gmmIntentUri = Uri.parse("geo:" + latitude + "," + longitude + "?q=" + latitude + "," + longitude + "(Label+Peta)");
+    private void openGoogleMaps() {
+        String googleMapsUrl = "https://maps.google.com/maps?q=-7.74777128178355,112.73866026944174";
 
-        // Membuat intent untuk Google Maps
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
+        Uri uri = Uri.parse(googleMapsUrl);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
 
-        // Memeriksa apakah aplikasi Google Maps terpasang
         PackageManager packageManager = requireActivity().getPackageManager();
-        List<ResolveInfo> activities = packageManager.queryIntentActivities(mapIntent, 0);
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
         boolean isIntentSafe = activities.size() > 0;
 
-        // Menampilkan daftar aplikasi yang bisa menangani intent
-        for (ResolveInfo resolveInfo : activities) {
-            String packageName = resolveInfo.activityInfo.packageName;
-            String activityName = resolveInfo.activityInfo.name;
-            Log.d("IntentHandler", "Found app to handle intent: " + packageName + ", " + activityName);
-        }
-
-        // Jika aplikasi Google Maps terpasang, maka buka alamat tersebut
         if (isIntentSafe) {
-            startActivity(mapIntent);
+            startActivity(intent);
         } else {
             Toast.makeText(requireContext(), "Aplikasi Google Maps tidak terpasang.", Toast.LENGTH_SHORT).show();
         }
